5c618556d9dfe220c99b6b5b1e802b756bc12d1a
Jordan Lee Osier <jordan.osier@skillmaple.com>
2023-02-25 11:16:42 -0500

    Add actual shell script for robots

--- build-robots-txt.sh
+++ build-robots-txt.sh
@@ -1,1 +1,14 @@
-echo 'something'
+#!/bin/bash
+
+if [[ $NODE_ENV = "production" ]]
+then
+    cat << EOF > .next/robots.txt
+User-agent: *
+Disallow:
+EOF
+else
+    cat << EOF > .next/robots.txt
+User-agent: *
+Disallow: /
+EOF
+fi
